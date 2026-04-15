5fcc965ccbc0d9c92c4a3629d836adf83c250efa
jumpServeDB <dbalint88@proton.me>
2026-04-15 11:56:13 +0100

    remove dummy data, fix min height

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
         <main
           id="main"
           className={clsx(
-            "flex flex-col items-center h-full",
+            "flex flex-col items-center h-full min-h-screen",
             isDarkMode
               ? "bg-slate-800 text-orange-200"
               : "text-slate-800 bg-orange-50",
@@ -40,12 +40,6 @@ const App = () => {
           <h1 className="text-4xl my-4">Lightsketch</h1>
           <ControlPanel />
           <CardHolder />
-          <LogoCard
-            description="1Password helps you keep your digital life secure by storing passwords, credit card details, and documents in an encrypted vault. Generate strong passwords, access your data from any device, and enjoy peace of mind with 1Password\\'s robust security features."
-            title="Secure your digital life"
-            logo="https://logotypes.dev/static/logos/1password-glyph-color.svg"
-            name="1password"
-          />
         </main>
       </ThemeContext.Provider>
     </MemoContext.Provider>
